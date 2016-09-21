6e772fcb9292def4197110a7b0c8fbfb34efe1e0
mobile12345 <sogeraniotaki@team.mobile.de>
2016-09-21 09:34:29 +0200

    Update Geodata.js

--- Geodata.js
+++ Geodata.js
@@ -37,8 +37,8 @@
             for (var i = 0, len = feat.length; i < len; i++) {
                 tableData.push({
 					"id": feat[i].id,
-                    "longitude": feat[i].longitude,
-                    "latitude": feat[i].latitude
+                    "longitude": feat[i].lon,
+                    "latitude": feat[i].lat
                 });
             }
 
